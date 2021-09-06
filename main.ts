f8ecc30a936ce2931426d7cd82acc202ce62b787
Pedro Cortez <cortez1098@HP-Laptop-15-da0xxx-aa2c60a3.localdomain>
2021-09-06 01:17:26 -0600

    Initial commit

---
+++ main.ts
@@ -0,0 +1,8 @@
+const rectangulo = { altura: 10, anchura: 15};
+const area = rectangulo.anchura * rectangulo.altura;
+
+function saludar(nombre){
+    return "Hola, " + nombre;
+}
+
+console.log(saludar("Pedro"));
